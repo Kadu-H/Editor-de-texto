5b2569a5bc5324b71c9ec25c53c7215ff06ed320
Kdu <olocgame@gmail.com>
2023-04-27 19:04:22 -0300

    Função de criar arquivo

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <fstream>
 #include <sstream>
 
+
+int CreateFile(std::string nameFile, std::string conteudo){
+
+    std::ofstream arquivo;
+    arquivo.open(nameFile,std::ios::out);
+
+    if(arquivo.is_open()){
+        arquivo << conteudo;
+        arquivo.close();
+    }
+
+    return 0;
+}
+
 int main(){
-    
+
+    std::string nameFile = "";
+    std::string conteudo = "";
+
+    CreateFile(nameFile,conteudo);
+
     return 0;
 }
